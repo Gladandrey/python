6832266083a802893754b3fc4ea7be8be80c3164
Andrey <gladandrey@gmail.com>
2021-04-07 16:08:06 +0300

    Change in the code

--- dz1_list.py
+++ dz1_list.py
@@ -1,6 +1,18 @@
 number = [i**3 for i in range(1,1001,2)]
 print(number)
+print()
 
+def sum_digits(value):
+    res = 0
+    while value != 0:
+        res += value % 10
+        value //= 10
+    return res
+res1 = sum(filter(lambda num: sum_digits(num) % 7 == 0, number))
+res2 = sum(filter(lambda num: sum_digits(num + 17) % 7 == 0, number))
+print(res1)
+print(res2)
+print()
 
 print(sum(filter(lambda j: sum(map(int, str(j))) % 7 == 0, number)))
 print(sum(filter(lambda j: sum(map(int, str(j + 17))) % 7 == 0, number)))
