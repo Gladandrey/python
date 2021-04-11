b0d47b4330ebd5c8ea23525c98ee01cd7ca469e1
gladandrey <gladandrey@gmail.com>
2021-04-11 20:46:47 +0300

    dz2

---
+++ dz2_1.py
@@ -0,0 +1,14 @@
+#1. Выяснить тип результата выражений:
+#15 * 3
+#15 / 3
+#15 // 2
+#15 ** 2
+
+a=15 * 3
+print(type(a)) #<class 'int'>
+a=15 / 3
+print(type(a)) #<class 'float'>
+a=15 // 2
+print(type(a)) #<class 'int'>
+a=15 ** 2
+print(type(a)) #<class 'int'>
